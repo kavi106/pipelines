4819d48ddceab88b18584be874421e21a3d880a2
Kavi Seewoogoolam <kavi@seewoogoolam.com>
2023-11-07 03:58:50 +0400

    WIP

--- VxBeCard/Phase-I/prechecks.py
+++ VxBeCard/Phase-I/prechecks.py
@@ -2,8 +2,8 @@ from time import sleep
 
 def excelValidation(**kwargs):
     sleep(5)
-    return 200, "Success", {"xx": kwargs}
+    return 200, "Success", {"xx": "excelValidation"}
 
 def myLabDataValidation(**kwargs):
     sleep(5)
-    return 400, "Failure", {"xx": kwargs}
+    return 400, "Failure", {"xx": "myLabDataValidation"}
