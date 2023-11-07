3561102ed1b58d76fee114309d172ec1bf2e560f
Kavi Seewoogoolam <kavi@seewoogoolam.com>
2023-11-07 13:41:40 +0400

    wip

--- VxBeCard/Phase-I/prechecks.py
+++ VxBeCard/Phase-I/prechecks.py
@@ -2,7 +2,7 @@ from time import sleep
 
 def excelValidation(**kwargs):
     sleep(5)
-    return 400, "All entered fields match the excel metadata from MyLabData.", {"xx": "excelValidation", "bb": {"xx": 1, "yy": 2}}
+    return 200, "All entered fields match the excel metadata from MyLabData.", {"xx": "excelValidation", "bb": {"xx": 1, "yy": 2}}
 
 def myLabDataValidation(**kwargs):
     sleep(5)
