9dfc694fd3177ca3910670d28e6bdf53d1d772c4
Kavi Seewoogoolam <kavi@seewoogoolam.com>
2023-11-07 10:18:46 +0400

    wip

--- VxBeCard/Phase-I/prechecks.py
+++ VxBeCard/Phase-I/prechecks.py
@@ -2,12 +2,12 @@ from time import sleep
 
 def excelValidation(**kwargs):
     sleep(5)
-    return 200, "Success", {"xx": "excelValidation", "bb": {"xx": 1, "yy": 2}}
+    return 200, "All entered fields match the excel metadata from MyLabData.", {"xx": "excelValidation", "bb": {"xx": 1, "yy": 2}}
 
 def myLabDataValidation(**kwargs):
     sleep(5)
-    return 400, "Failure", {"xx": "myLabDataValidation", "cc": "XYZ"}
+    return 400, "0 fsc files found !", {"xx": "myLabDataValidation", "cc": "XYZ"}
 
 def testValidation(**kwargs):
     sleep(3)
-    return 200, "Success", {}
+    return 200, "Success !!!", {}
