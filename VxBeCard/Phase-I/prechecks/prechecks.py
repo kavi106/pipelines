9ac424c1be5a21c57b89d58514c6b9fd7821cf75
Kavi Seewoogoolam <kavi@seewoogoolam.com>
2023-11-07 18:15:46 +0400

    wip

--- VxBeCard/Phase-I/prechecks/prechecks.py
+++ VxBeCard/Phase-I/prechecks/prechecks.py
@@ -4,12 +4,14 @@ def pullConfigFiles(**kwargs):
     sleep(5)
     kwargs["xx"] = "excelValidation"
     kwargs["bb"] = {"xx": 1, "yy": 2}
+    print(kwargs)
     return 200, "All entered fields match the excel metadata from MyLabData.", kwargs
 
 def pullFcsFiles(**kwargs):
     sleep(5)
-    kwargs["xx"] = "myLabDataValidation"
+    kwargs["ws"] = "myLabDataValidation"
     kwargs["cc"] = "XYZ"
+    print(kwargs)
     return 400, "0 fsc files found !", kwargs
 
 def validateExcelMetadataFile(**kwargs):
