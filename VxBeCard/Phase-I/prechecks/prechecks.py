36b17eb5d585c4f38069efbe42d16b215d4dc56f
Kavi Seewoogoolam <kavi@seewoogoolam.com>
2023-11-08 23:21:22 +0400

    wip

--- VxBeCard/Phase-I/prechecks/prechecks.py
+++ VxBeCard/Phase-I/prechecks/prechecks.py
@@ -48,6 +48,7 @@ def pull_config_files(input_dict):
 
 
 def pull_fcs_files(input_dict):
+    return 200, f"All fsc files found !", input_dict
     """_summary_
 
     Args:
@@ -130,7 +131,7 @@ def _get_file_list(input_dict, file_pattern, uftype):
 
 def validate_meta_data_excel(input_dict):
     #print(input_dict)
-    #return 200, "All meta data fields match excel sheet", input_dict
+    return 200, "All meta data fields match excel sheet", input_dict
     """Validate Meta Data Excel entries agains input_dict based on a validation_json.
 
     Args:
