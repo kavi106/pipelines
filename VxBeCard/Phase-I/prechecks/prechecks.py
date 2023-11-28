691938e6b0595889866584fc36c0d13ce2d63514
Kavi Seewoogoolam <kavi@seewoogoolam.com>
2023-11-28 11:48:16 +0400

    wip

--- VxBeCard/Phase-I/prechecks/prechecks.py
+++ VxBeCard/Phase-I/prechecks/prechecks.py
@@ -68,10 +68,12 @@ def sanitizing_user_inputs(input_dict):
     for field in inputs:
         input_type = inputs[field]["type"]
         if input_type == "string":
-            return (400, f"{inputs[field]['pattern']}", input_dict)
-            if not re.match(inputs[field]['pattern'], input_dict[field]):
-                input_dict[field] = ""
-                output.append(f"{inputs[field]['name']} has invalid input.")
+            try:
+                if not re.match(inputs[field]['pattern'], input_dict[field]):
+                    input_dict[field] = ""
+                    output.append(f"{inputs[field]['name']} has invalid input.")
+            except:
+                return (400, f"{inputs[field]['pattern']}", input_dict)
 
     if len(output) > 0:
         error_message = ", ".join(output)
