a2c27e66b9dfb1ae257f155ade40261f74379cb2
Kavi Seewoogoolam <kavi@seewoogoolam.com>
2023-12-04 09:57:29 +0400

    wip

--- VxBeCard/Phase-I/prechecks/prechecks.py
+++ VxBeCard/Phase-I/prechecks/prechecks.py
@@ -4,7 +4,6 @@ import os
 import re
 import logging
 import requests
-#import datetime
 import ursgal
 import github.GithubException
 import pandas as pd
@@ -21,89 +20,61 @@ from collections import Counter
 
 pd.set_option("display.max_columns", 100)
 
+
 def sanitizing_user_inputs(input_dict):
-    inputs = {
-        "myLabDataTaskId": {
-            "name" : "MyLabData TaskId",
-            "pattern": "^[A-Za-z0-9_-]{0,30}$",
-            "type": "string"
-        },
-        "requesterMUDID": {
-            "name" : "Requester MUDID",
-            "pattern": "^[A-Za-z0-9]{0,15}$",
-            "type": "string"
-        },
-        "instrumentSapId": {
-            "name" : "Instrument SAP ID",
-            "pattern": "^[0-9]{0,20}$",
-            "type": "string"
-        },
-        "dataCleanAlgorithm": {
-            "name" : "Data Clean Algorithm",
-            "pattern": "^.{0,20}$",
-            "type": "string"
-        },
-        "projectName": {
-            "name" : "Project Name",
-            "pattern": "^.{0,50}$",
-            "type": "string"
-        },
-        "experimentNumber": {
-            "name" : "Experiment Number",
-            "pattern": "^ELN[0-9a-zA-Z-_]{0,10}$",
-            "type": "string"
-        },
-        "studyNumber": {
-            "name" : "Study Number",
-            "pattern": "^[0-9a-zA-Z_-]{0,30}$",
-            "type": "string"
-        },
-        "resultRecipient": {
-            "name" : "Result Recipient",
-            "pattern": "^[a-zA-Z0-9_.]+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$",
-            "type": "RecipientArray"
-        }
-    }
+    auth = Auth.Token(os.getenv("CONFIG_REPO_TOKEN"))
+    g = Github(auth=auth)
+    repo = g.get_repo(os.getenv("CONFIG_REPO"))
+
+    try:
+        file_content = repo.get_contents(
+            "jsonforms/precheck_rules.json", os.getenv("CONFIG_REPO_BRANCH")
+        )
+    except github.GithubException:
+        return update_validation(
+            400,
+            "Cannot get configuration files jsonforms/precheck_rules.json from branch {os.getenv('CONFIG_REPO_BRANCH')}!",
+            input_dict
+        )
+    validtion_rules = json.loads(file_content.decoded_content.decode())
     output = []
-    for field in inputs:
-        input_type = inputs[field]["type"]
+    for field in validtion_rules:
+        input_type = validtion_rules[field]["type"]
         if input_type == "string":
-            if field in input_dict and not re.match(inputs[field]['pattern'], str(input_dict[field])):
+            if field in input_dict and not re.match(
+                validtion_rules[field]["pattern"], str(input_dict[field])
+            ):
                 input_dict[field] = ""
-                output.append(f"{inputs[field]['name']} has invalid input.")
+                output.append(f"{validtion_rules[field]['name']} has invalid input.")
         elif field in input_dict and input_type == "RecipientArray":
-             for email in input_dict[field]:
-                 if not re.match(inputs[field]['pattern'], str(email['email'])):
+            for email in input_dict[field]:
+                if not re.match(validtion_rules[field]["pattern"], str(email["email"])):
                     input_dict[field] = None
-                    output.append(f"{inputs[field]['name']} is invalid.")
+                    output.append(f"{validtion_rules[field]['name']} is invalid.")
                     input_dict[field] = []
                     break
 
     if len(output) > 0:
         error_message = ", ".join(output)
-        return (400, error_message, update_input_dict(input_dict, error_message))
+        return update_validation(400, error_message, input_dict)
     else:
         return (200, "All input fields are valid.", input_dict)
 
 
-def update_input_dict(input_dict, error_message):
-    input_dict["_validation"] = 400
-    input_dict["_validation_message"].append(error_message)
-    return input_dict
+def update_validation(code, message, input_dict):
+    if code == 400:
+        input_dict["_validation"] = 400
+        input_dict["_validation_message"].append(message)
+
+    return (code, message, input_dict)
 
 
 def list_mylabdata_file(input_dict):
     _init_ursgal(input_dict)
-    # ursgal.instances.ucredential_manager.add_credentials(
-    #     input_dict["exits"][0]["kwargs"]["ursgal_credentials"]["credentials_lookup"]
-    # )
     equipment_id = input_dict["instrumentSapId"]
     task_id = input_dict["myLabDataTaskId"]
     storage_base = f"mylabdata://{input_dict['prechecks_config']['mylabdata_api_backend_url']}/{equipment_id}/{task_id}"
 
-    # ursgal.config["certificates"][
-    #     input_dict["prechecks_config"]["mylabdata_api_backend_url"]
-    # ] = False
     input_dict["mylabdata_files"] = []
 
     try:
@@ -111,13 +82,15 @@ def list_mylabdata_file(input_dict):
             f"{storage_base}#dummy.txt"
         ).io.list_container_items()
     except:
-        return (400, f"Cannot connect to {storage_base}", input_dict)
+        return update_validation(400, f"Cannot connect to {storage_base}", input_dict)
 
     input_dict["mylabdata_files"] = file_list_mld
 
     return_code = 400 if len(file_list_mld) == 0 else 200
 
-    return return_code, f"Found {len(file_list_mld)} files in mylabdata bucket", input_dict
+    return update_validation(
+        return_code, f"Found {len(file_list_mld)} files in mylabdata bucket", input_dict
+    )
 
 
 def pull_config_files(input_dict):
@@ -159,7 +132,7 @@ def pull_config_files(input_dict):
                     f"{folder}/{filename}", os.getenv("CONFIG_REPO_BRANCH")
                 )
             except github.GithubException:
-                return (
+                return update_validation(
                     400,
                     f"Cannot get configuration files {folder}/{filename} from branch {os.getenv('CONFIG_REPO_BRANCH')}!",
                     input_dict,
@@ -176,7 +149,7 @@ def pull_config_files(input_dict):
             meta_data_excel_mapping_location, os.getenv("CONFIG_REPO_BRANCH")
         ).decoded_content.decode()
     except github.GithubException:
-        return (
+        return update_validation(
             400,
             f"Cannot get meta data file, expected: {meta_data_excel_mapping_location}!",
             input_dict,
@@ -204,12 +177,16 @@ def pull_fcs_files(input_dict):
 
         len_fcs_file_list = len(fcs_uri_list)
         if len_fcs_file_list == 0:
-            return 400, f"0 fsc files under TaskId {input_dict['myLabDataTaskId']}. Please double check if the TaskId is correct.", input_dict
+            return update_validation(
+                400,
+                f"0 fsc files under TaskId {input_dict['myLabDataTaskId']}. Please double check if the TaskId is correct.",
+                input_dict,
+            )
 
         input_dict["fcs_uri_list"] = fcs_uri_list
 
     except:
-        return 400, f"Error getting fcs files !", input_dict
+        return update_validation(400, f"Error getting fcs files !", input_dict)
 
     return 200, f"{len_fcs_file_list} fsc files found !", input_dict
 
@@ -241,7 +218,7 @@ def _get_file_list(input_dict, file_pattern, uftype):
             f"{storage_base}#dummy.txt"
         ).io.list_container_items()
     except:
-        return (400, f"Cannot connect to {storage_base}", input_dict)
+        return update_validation(400, f"Cannot connect to {storage_base}", input_dict)
 
     file_list = []
     for file in file_list_mld:
@@ -259,7 +236,9 @@ def _init_ursgal(input_dict):
             input_dict["prechecks_config"]["mylabdata_api_backend_url"]
         ] = False
     except:
-        return 400, "Failed to initialize ursgal ucredentials !", input_dict
+        return update_validation(
+            400, "Failed to initialize ursgal ucredentials !", input_dict
+        )
 
 
 def validate_meta_data_excel(input_dict):
@@ -284,21 +263,29 @@ def validate_meta_data_excel(input_dict):
             uftype=ursgal.uftypes.mx.METADATA_XLSX,
         )
     except:
-        return 400, f"Error getting Excel Metadata file !", input_dict
+        return update_validation(
+            400, f"Error getting Excel Metadata file !", input_dict
+        )
 
     num_excel_files = len(excel_files)
     if num_excel_files == 0:
-        return (
-            400, 
+        return update_validation(
+            400,
             "0 excel metadata file found !"
             " Please make sure that the excel filename is"
-            " 'standard_metadata_lab_input_v1.0.0.xlsx'", 
+            " 'standard_metadata_lab_input_v1.0.0.xlsx'",
             input_dict
         )
 
     if num_excel_files != 1:
-        e_file_names = ', '.join([(ursgal.UFile(file)).object_name for file in excel_files])
-        return 400, f"{num_excel_files} metadata excel files found in the main folder: ({e_file_names}) !", input_dict
+        e_file_names = ", ".join(
+            [(ursgal.UFile(file)).object_name for file in excel_files]
+        )
+        return update_validation(
+            400,
+            f"{num_excel_files} metadata excel files found in the main folder: ({e_file_names}) !",
+            input_dict
+        )
 
     meta_excel_uf = ursgal.UFile(excel_files[0])
     meta_xls = pd.ExcelFile(meta_excel_uf.path)
@@ -319,7 +306,8 @@ def validate_meta_data_excel(input_dict):
         check_requires_validation = check.get("validate", True)
 
         if (
-            str(check["meta_data_excel_value"]).strip() == str(check["input_value"]).strip()
+            str(check["meta_data_excel_value"]).strip()
+            == str(check["input_value"]).strip()
             or check_requires_validation is False
         ):
             input_dict["validated_meta_data"][json_field] = check[
@@ -334,7 +322,7 @@ def validate_meta_data_excel(input_dict):
                 )
             )
     if len(msg) > 0:
-        return 400, ". ".join(msg), input_dict
+        return update_validation(400, ". ".join(msg), input_dict)
     else:
         return 200, "All meta data fields match excel sheet", input_dict
 
@@ -361,17 +349,17 @@ def validate_plate_csv(input_dict):
             uftype=ursgal.uftypes.any.CSV,
         )
     except:
-        return 400, "Error getting CSV file !", input_dict
+        return update_validation(400, "Error getting CSV file !", input_dict)
 
     if len(plate_csvs) == 0:
-        return (
+        return update_validation(
             400,
             "0 plate CSV file found in MyLabData for the input TaskId"
             " Please make sure that the csv filename follows the naming convention:"
-            " 'standard_metadata_lab_input_PlateX_v1.0.0.csv' where X is an integer", 
-            input_dict,
+            " 'standard_metadata_lab_input_PlateX_v1.0.0.csv' where X is an integer",
+            input_dict
         )
-    
+
     panel_names = []
     logging.debug(f"will check agains {input_dict['validated_meta_data']}")
     for p_csv in plate_csvs:
@@ -380,10 +368,10 @@ def validate_plate_csv(input_dict):
         for j_field, c_field in jsonforms_to_csv_column_name_mappings.items():
             if df[c_field].nunique() != 1:
                 logging.debug(df.head(10))
-                return (
+                return update_validation(
                     400,
                     f"Plate CSV {p_csv_uf.object_name} has multiple entries in {c_field}, expected only 1",
-                    input_dict,
+                    input_dict
                 )
             c_value = df[c_field].unique()[0]
             j_value = input_dict["validated_meta_data"].get(
@@ -392,78 +380,79 @@ def validate_plate_csv(input_dict):
             )
             if c_value != j_value:
                 logging.debug(df.head(10))
-                return (
+                return update_validation(
                     400,
                     f"Plate CSV {p_csv_uf.object_name} has {c_value} in {c_field},"
                     f" yet extracted value {j_value} from meta data excel sheet.",
-                    input_dict,
+                    input_dict
                 )
             else:
                 logging.debug(f"{j_value} matches {c_field}")
 
-        if not hasattr(df, 'panel_name'):
-            return (
-                400, 
-                f"Plate CSV {p_csv_uf.object_name} does not have panel name field !", 
+        if not hasattr(df, "panel_name"):
+            return update_validation(
+                400,
+                f"Plate CSV {p_csv_uf.object_name} does not have panel name field !",
                 input_dict
             )
 
-        if df['panel_name'].nunique() != 1:
+        if df["panel_name"].nunique() != 1:
             logging.debug(df.head(10))
-            return (
+            return update_validation(
                 400,
                 f"Plate CSV {p_csv_uf.object_name} has multiple entries in panel_name, expected only 1",
-                input_dict,
+                input_dict
             )
         else:
-            panel_names.append(df['panel_name'].unique()[0])
+            panel_names.append(df["panel_name"].unique()[0])
 
     input_dict["panel_names"] = panel_names
-    
+
     return (
         200,
         "All Plate CSV entries for experiment_number and"
         " experiment_name match meta_data.xlsx and user input.",
-        input_dict,
+        input_dict
     )
 
-def validate_panel_name(input_dict):
-    """Validate panel name in CSV file match plate name in fcs file name.
 
-    """
+def validate_panel_name(input_dict):
+    """Validate panel name in CSV file match plate name in fcs file name."""
     unique_panel_names = list(dict.fromkeys(input_dict["panel_names"]))
     num_panel_names = len(unique_panel_names)
     if num_panel_names > 1:
-        return (
-            400, 
-            f"{num_panel_names} panel names found in CSV files ({', '.join(unique_panel_names)}) !", 
+        return update_validation(
+            400,
+            f"{num_panel_names} panel names found in CSV files ({', '.join(unique_panel_names)}) !",
             input_dict
         )
-    
+
     panel_name = unique_panel_names[0]
-    p_fcs_file = ursgal.UFile(input_dict['fcs_uri_list'][0])
-    panel_name_fcs_file = (p_fcs_file.object_name.split('/')[-1]).split('_')[2]
+    p_fcs_file = ursgal.UFile(input_dict["fcs_uri_list"][0])
+    panel_name_fcs_file = (p_fcs_file.object_name.split("/")[-1]).split("_")[2]
     if panel_name.strip() != panel_name_fcs_file.strip():
-        return (
+        return update_validation(
             400,
             f"Plate CSV has {panel_name} as panel name,"
             f" yet fcs file has {panel_name_fcs_file} as panel name.",
-            input_dict,
+            input_dict
         )
-    
+
     return (
         200,
         "Panel name in CSV files match panel name in fcs file name.",
-        input_dict,
+        input_dict
     )
 
 
 def start_prefect_pipeline(input_dict):
     wid = ursgal.UWIDGenerator().generate_wid()
     input_dict["wid"] = wid
-    
+
     if "_validation" in input_dict and input_dict["_validation"] != 200:
-        return 400, "Please fix the errors and submit again !", input_dict
+        return update_validation(
+            400, "Please fix the errors and submit again !", input_dict
+        )
 
     try:
         configuration_json = input_dict["exits"][0]["kwargs"]["base_input_json"]
@@ -472,8 +461,8 @@ def start_prefect_pipeline(input_dict):
             "ursgal_credentials"
         ]["credentials_lookup"]
     except:
-        return 400, "No fcs files to process !", input_dict
-    
+        return update_validation(400, "No fcs files to process !", input_dict)
+
     response = requests.post(
         url=input_dict["exits"][0]["kwargs"]["prefect_netloc"],
         json={
@@ -497,7 +486,8 @@ def send_notification(input_dict):
     try:
         recipients = (
             [i["email"] for i in input_dict["resultRecipient"]]
-            if "resultRecipient" in input_dict and len(input_dict["resultRecipient"]) > 0
+            if "resultRecipient" in input_dict
+            and len(input_dict["resultRecipient"]) > 0
             else []
         )
         recipients.append(input_dict["requesterEmail"])
@@ -516,7 +506,7 @@ def send_notification(input_dict):
         message_text = "Task submitted successfully."
 
     date_time_str = datetime.now().strftime("%Y-%m-%d:%H:%M:%S")
-    task_id = input_dict['myLabDataTaskId']
+    task_id = input_dict["myLabDataTaskId"]
     if len(message_text) > 0:
         templ_variables = {
             "date_time_str": date_time_str,
@@ -524,7 +514,7 @@ def send_notification(input_dict):
             "task_id": task_id,
             "message_text": message_text,
             "message_color": message_color,
-            "wid": input_dict['wid'],
+            "wid": input_dict["wid"],
         }
         html = render_template(email_template, **templ_variables)
         send_email(
@@ -541,7 +531,7 @@ def send_notification(input_dict):
             crud.create_recipient(
                 db=db,
                 recipient={
-                    "wid": input_dict['wid'],
+                    "wid": input_dict["wid"],
                     "recipients": ",".join(recipients),
                     "taskid": f"{task_id}",
                     "instrumentid": f"{input_dict['instrumentSapId']}",
@@ -554,6 +544,7 @@ def send_notification(input_dict):
         return 400, "Error saving to database !", {}
 
     return 200, "Email sent successfully !", {}
+
 
 def main(input_json):
     logging.info(f"Launched with {input_json}")
